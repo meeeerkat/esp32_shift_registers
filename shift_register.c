1cee7b26e4a13fce8ea25730b6f2184b5067457a
Paul Suau <paul.suau@grenoble-inp.org>
2023-02-04 01:32:37 +0000

    Reset shift register to 0 on init

--- shift_register.c
+++ shift_register.c
@@ -26,9 +26,8 @@ void shift_register__init(shift_register_t* sr, uint8_t ser, uint8_t srclk, uint
   gpio_set_level(srclk, 0);
   gpio_set_level(rclk, 0);
 
-  // Maybe reset register ???
-  // using a new reset pin or just setting it all at 0 ???
-  // right now bits_sent isn't right before the first send
+  // Reset register
+  shift_register__send(sr, 0);
 }
 
 void shift_register__send(shift_register_t* sr, uint64_t bits) {
